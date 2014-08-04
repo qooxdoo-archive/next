1e73f4da67f1a2ea28defe7197b687b78bc73778
wittemann <martin.wittemann@1und1.de>
2014-08-04 12:56:49 +0200

    [BUG #8511] Minor

--- framework/source/class/qx/module/Storage.js
+++ framework/source/class/qx/module/Storage.js
@@ -20,9 +20,7 @@
 /**
  * This module offers a cross browser storage implementation. The API is aligned
  * with the API of the HTML web storage (http://www.w3.org/TR/webstorage/) which is
- * also the preferred implementation used. As fallback for IE < 8, we use user data.
- * If both techniques are unsupported, we supply a in memory storage, which is
- * of course, not persistent.
+ * also the preferred implementation used.
  */
 qx.Bootstrap.define("qx.module.Storage", {
   statics :
