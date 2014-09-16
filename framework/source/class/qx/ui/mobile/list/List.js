6f7461d6c4530e732cd83e36d28b121cb4c6e97c
danielwagner <daniel.wagner@1und1.de>
2014-09-16 13:08:54 +0200

    [BUG #8560] fixed item minHeight (can't call setStyle on a documentFragment)

--- framework/source/class/qx/ui/mobile/list/List.js
+++ framework/source/class/qx/ui/mobile/list/List.js
@@ -494,7 +494,7 @@ qx.Bootstrap.define("qx.ui.mobile.list.List",
           itemHeight = this.itemHeight + "px";
         }
         // Fixed height
-        itemElement.setStyle("minHeight", itemHeight)
+        qxWeb(itemElement[0].childNodes[0]).setStyle("minHeight", itemHeight)
           .setStyle("maxHeight", itemHeight);
 
         this.append(itemElement);
