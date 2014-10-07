e68862e0ab3070f3b954b7e0f5abfe06d0479310
danielwagner <daniel.wagner@1und1.de>
2014-10-07 09:27:30 +0200

    [BUG #8606] Created UI module

---
+++ framework/source/class/qx/module/Ui.js
@@ -0,0 +1,39 @@
+"use strict";
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2014 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+************************************************************************ */
+
+/**
+ * Placeholder class which requires all widgets.
+ *
+ * @require(qx.ui.mobile.Button)
+ * @require(qx.ui.mobile.Rating)
+ * @require(qx.ui.mobile.Widget)
+ * @require(qx.ui.mobile.basic.Image)
+ * @require(qx.ui.mobile.container.Carousel)
+ * @require(qx.ui.mobile.container.Collapsible)
+ * @require(qx.ui.mobile.control.Calendar)
+ * @require(qx.ui.mobile.control.DatePicker)
+ * @require(qx.ui.mobile.control.Table)
+ * @require(qx.ui.mobile.form.CheckBox)
+ * @require(qx.ui.mobile.form.Slider)
+ * @require(qx.ui.mobile.list.List)
+ * @require(qx.ui.mobile.tabbar.TabBar)
+ * @require(qx.ui.mobile.tree.Tree)
+ * @require(qx.ui.mobile.layout.Abstract)
+ * @require(qx.ui.mobile.layout.AbstractBox)
+ * @require(qx.ui.mobile.layout.HBox)
+ * @require(qx.ui.mobile.layout.VBox)
+ */
