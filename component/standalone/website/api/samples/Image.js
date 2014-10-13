9b991f90d61f6fcd55247c17102bcd7fed1a8a16
tobiasoberrauch <tobias.oberrauch@1und1.de>
2014-10-13 15:37:00 +0200

    [BUG #8625] Add examples for basic.Image

---
+++ component/standalone/website/api/samples/Image.js
@@ -0,0 +1,17 @@
+addSample('.image', {
+  html: '<img id="image-example" src="http://qooxdoo.org/_media/desktop.png" />',
+  javascript: function () {
+    q('#image-example').image();
+  },
+  executable: true,
+  showMarkup: true
+});
+
+addSample('.image', {
+  javascript: function () {
+    var image = new qx.ui.mobile.basic.Image("http://qooxdoo.org/_media/desktop.png");
+    q(document.body).append(image);
+  },
+  executable: true,
+  showMarkup: true
+});
