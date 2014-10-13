0bf9286722e87ed0cb7f30df9fc6c312d5f7e621
tobiasoberrauch <tobias.oberrauch@1und1.de>
2014-10-13 13:01:49 +0200

    [BUG #8625] Add examples for date picker

--- component/standalone/website/api/samples/DatePicker.js
+++ component/standalone/website/api/samples/DatePicker.js
@@ -1,53 +1,23 @@
 addSample(".datepicker", {
   html : ['<input type="text" id="datepicker-example"/>'],
   javascript: function() {
-q("#datepicker-example").datePicker(new Date());
+q("#datepicker-example").datePicker();
   },
   executable: true,
   showMarkup: true
 });
 
 addSample(".datepicker", {
-  html : ['<input type="text" id="datepicker-example"/>'],
   javascript: function() {
-var datePicker = q("#datepicker-example").datePicker(new Date());
-// customize the format function to change the value which is
-// set to the input element
-datePicker.set({
-  format: function (date) {
-    return date.toLocaleString();
-  }
-});
-  },
-  executable: true,
-  showMarkup: true
-});
-
-addSample(".datepicker", {
-  html : ['<input type="text" id="datepicker-example"/>'],
-  javascript: function() {
-var datePicker = q("#datepicker-example").datePicker(new Date());
-
-// configure calendar widget
-// only allow to select week days and only allow to select today and future days
-datePicker.getCalendar().set({
-  selectableWeekdays: [1, 2, 3, 4, 5],
-  minDate: new Date()
-});
-
-  },
-  executable: true,
-  showMarkup: true
-});
-
-addSample(".datepicker", {
-  html : ['<input type="text" id="datepicker-example"/>'],
-  javascript: function() {
-var datePicker = q("#datepicker-example").datePicker(new Date());
-
-// allow user input on the connected input element
+var datePicker = new qx.ui.mobile.control.DatePicker(new Date());
+// enable to manipulate input field
 datePicker.set({
   readonly: false
+});
+// only allow to select week days and only allow to select today and future days
+datePicker.getCalendar().set({
+  selectableWeekDays: [1, 2, 3, 4, 5],
+  minDate: new Date()
 });
   },
   executable: true,
