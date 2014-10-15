5523e572b3e0a55f436b334490f7509c948afad2
tobiasoberrauch <tobias.oberrauch@1und1.de>
2014-10-15 18:23:09 +0200

    [BUG #8609] Add examples for drawer widget

---
+++ component/standalone/website/api/samples/Drawer.js
@@ -0,0 +1,27 @@
+addSample(".drawer", {
+  html : ['<div id="drawer-example"></div>'],
+  javascript: function() {
+    var drawer = q('#drawer-example').drawer();
+
+    // only to show the drawer by clicking on document body
+    q(document.body).on("tap", function () {
+      drawer.isHidden() ? drawer.show() : drawer.hide();
+    });
+  },
+  executable: true,
+  showMarkup: true
+});
+
+addSample(".drawer", {
+  javascript: function() {
+    var drawer = new qx.ui.mobile.container.Drawer();
+    drawer.positionZ = "below";
+
+    // click on body to show the drawer
+    q(document.body).append(drawer).on("tap", function () {
+      drawer.isHidden() ? drawer.show() : drawer.hide();
+    });
+  },
+  executable: true,
+  showMarkup: true
+});
