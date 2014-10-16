0be79037d38105d64cf22638fcd3dba1173057db
tobiasoberrauch <tobias.oberrauch@1und1.de>
2014-10-16 10:44:08 +0200

    [BUG #8609] Add examples for atom widget

---
+++ component/standalone/website/api/samples/Atom.js
@@ -0,0 +1,18 @@
+addSample('.atom', {
+  html: '<div id="atom-example"></div>',
+  javascript: function () {
+    q('#atom-example').atom("qooxdoo desktop", "http://qooxdoo.org/_media/desktop.png");
+  },
+  executable: true,
+  showMarkup: true
+});
+
+addSample('.atom', {
+  javascript: function () {
+    var atom = new qx.ui.mobile.basic.Atom("qooxdoo desktop", "http://qooxdoo.org/_media/desktop.png");
+    atom.iconPosition = 'bottom';
+    q(document.body).append(atom);
+  },
+  executable: true,
+  showMarkup: true
+});
