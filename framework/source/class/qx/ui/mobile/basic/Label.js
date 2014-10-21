a23717af18c7bef23e12890986e68ae330766bd3
danielwagner <daniel.wagner@1und1.de>
2014-10-21 15:15:42 +0200

    [BUG #8609] removed factory method (conflict with Atom)

--- framework/source/class/qx/ui/mobile/basic/Label.js
+++ framework/source/class/qx/ui/mobile/basic/Label.js
@@ -162,10 +162,5 @@ qx.Class.define("qx.ui.mobile.basic.Label",
         qx.locale.Manager.getInstance().off("changeLocale", this._onChangeLocale, this);
       }
     }
-  },
-
-
-  classDefined : function(statics) {
-    qxWeb.$attachWidget(statics);
   }
 });
