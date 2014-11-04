4a0b1a1e6ac520d509ec285781705cc3fc46b69f
danielwagner <daniel.wagner@1und1.de>
2014-11-04 13:51:00 +0100

    [BUG #8637] fixes for property chains containing array indexes

--- framework/source/test/tests/framework/data/singlevalue/Resolve.js
+++ framework/source/test/tests/framework/data/singlevalue/Resolve.js
@@ -24,34 +24,45 @@ describe("data.singlevalue.Resolve", function() {
 
 
   it("ResolveDepth1", function() {
-      var model = qx.data.marshal.Json.createModel({a: 12});
-      assert.equal(12, qx.data.SingleValueBinding.resolvePropertyChain(model, "a"));
+    var model = qx.data.marshal.Json.createModel({a: 12});
+    assert.equal(12, qx.data.SingleValueBinding.resolvePropertyChain(model, "a"));
   });
 
 
   it("ResolveDepth2", function() {
-      var model = qx.data.marshal.Json.createModel({a: {b:12}});
-      assert.equal(12, qx.data.SingleValueBinding.resolvePropertyChain(model, "a.b"));
+    var model = qx.data.marshal.Json.createModel({a: {b:12}});
+    assert.equal(12, qx.data.SingleValueBinding.resolvePropertyChain(model, "a.b"));
   });
 
 
   it("ResolveDepthHuge", function() {
-      var model = qx.data.marshal.Json.createModel({a: {b: {c: {d: {e: {f: 12}}}}}});
-      assert.equal(12, qx.data.SingleValueBinding.resolvePropertyChain(model, "a.b.c.d.e.f"));
+    var model = qx.data.marshal.Json.createModel({a: {b: {c: {d: {e: {f: 12}}}}}});
+    assert.equal(12, qx.data.SingleValueBinding.resolvePropertyChain(model, "a.b.c.d.e.f"));
   });
 
 
   it("ResolveWithArray", function() {
-      var model = qx.data.marshal.Json.createModel({a: {b: [{c: 12}]}});
-      assert.equal(12, qx.data.SingleValueBinding.resolvePropertyChain(model, "a.b[0].c"));
+    var model = qx.data.marshal.Json.createModel({a: {b: [{c: 12}]}});
+    assert.equal(12, qx.data.SingleValueBinding.resolvePropertyChain(model, "a.b[0].c"));
   });
 
 
   it("ResolveNotExistant", function() {
-      var model = qx.data.marshal.Json.createModel({a: 12});
-      assert.throw(function() {
-        assert.equal(12, qx.data.SingleValueBinding.resolvePropertyChain(model, "b"));
-      });
+    var model = qx.data.marshal.Json.createModel({a: 12});
+    assert.throw(function() {
+      assert.equal(12, qx.data.SingleValueBinding.resolvePropertyChain(model, "b"));
+    });
   });
 
+
+  it("ResolveArrayIndex", function() {
+    var arr = new qx.data.Array([23]);
+    assert.equal(23, qx.data.SingleValueBinding.resolvePropertyChain(arr,"[0]"));
+  });
+
+
+  it("ResolveNestedArray", function() {
+    var model = qx.data.marshal.Json.createModel({a: [[23]]});
+    assert.equal(23, qx.data.SingleValueBinding.resolvePropertyChain(model, "a[0][0]"));
+  });
 });
