f4837be7a97ddfe406143dafbf9f5ed959c15d7d
tobiasoberrauch <tobias.oberrauch@1und1.de>
2014-11-05 16:29:07 +0100

    [BUG #8625] Add carousel samples to website api

---
+++ component/standalone/website/api/samples/Carousel.js
@@ -0,0 +1,32 @@
+addSample('.carousel', {
+  html: '<div id="carousel-example">' +
+    '<div class="qx-carousel-scroller">' +
+    '<div class="qx-carousel-page">asd</div>' +
+    '<div class="qx-carousel-page">dsa</div>' +
+    '</div>' +
+    '</div>',
+  javascript: function () {
+    q('#carousel-example').toCarousel();
+  },
+  executable: true,
+  showMarkup: true
+});
+
+addSample('.carousel', {
+  javascript: function () {
+    var carousel = new qx.ui.container.Carousel();
+
+    var frontPage = new qx.ui.Widget()
+      .append(new qx.ui.basic.Label("This is a carousel. Please swipe left."));
+
+    var secondPage = new qx.ui.Widget()
+      .append(new qx.ui.basic.Label("Now swipe right."));
+
+    carousel.append(frontPage);
+    carousel.append(secondPage);
+
+    q(document.body).append(carousel);
+  },
+  executable: true,
+  showMarkup: true
+});
