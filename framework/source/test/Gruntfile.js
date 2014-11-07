9b928812e0bb7b011b88655eb5b5b2a695e08ea0
danielwagner <daniel.wagner@1und1.de>
2014-11-07 11:01:52 +0100

    [BUG #8644] Added source version support for mocha-based tests

--- framework/source/test/Gruntfile.js
+++ framework/source/test/Gruntfile.js
@@ -13,32 +13,41 @@ module.exports = function(grunt) {
   // process test HTML
   grunt.registerTask('html', 'A task to preprocess the website.html', function(scope) {
     var testPath = getTestPath(scope);
-    // read index file
-    var index = fs.readFileSync('index.html', {encoding: 'utf8'});
+    // read index files
+    ["index.html", "index-source.html"].forEach(function(fileName) {
+      var index = fs.readFileSync(fileName, {encoding: 'utf8'});
 
-    var tests = glob.sync(testPath);
-    var scriptTags = '  <!-- TESTS START -->\n';
-    tests.forEach(function(path) {
-      // ignore all files starting with lower letter like setup.js
-      if (path.indexOf("setup.js") != -1) {
-        return;
-      }
-      scriptTags += '  <script src="' + path + '"></script>\n';
+      var tests = glob.sync(testPath);
+      var scriptTags = '  <!-- TESTS START -->\n';
+      tests.forEach(function(path) {
+        // ignore all files starting with lower letter like setup.js
+        if (path.indexOf("setup.js") != -1) {
+          return;
+        }
+        scriptTags += '  <script src="' + path + '"></script>\n';
+      });
+      scriptTags += '  <!-- TESTS END -->';
+
+      index = index.replace(/\s\s<!--\sTESTS\sSTART\s-->((.|\n)*)<!--\sTESTS\sEND\s-->/g, scriptTags);
+
+      // write index file
+      fs.writeFileSync(fileName, index, {'encoding': 'utf8'});
     });
-    scriptTags += '  <!-- TESTS END -->';
-
-    index = index.replace(/\s\s<!--\sTESTS\sSTART\s-->((.|\n)*)<!--\sTESTS\sEND\s-->/g, scriptTags);
-
-    // write index file
-    fs.writeFileSync('index.html', index, {'encoding': 'utf8'});
   });
 
 
   var shell = require('shelljs');
-  grunt.registerTask('build', 'Build the test artefact', function() {
-    shell.cd('../../');
+  grunt.registerTask('build', 'Build the test artifact (optimized version)', function() {
     console.log('Opening the framework folder');
+    shell.cd('../../');
     shell.exec('grunt build-all');
+    shell.cd('source/test');
+  });
+
+  grunt.registerTask('source', 'Build the test artifact (source version)', function() {
+    console.log('Opening the framework folder');
+    shell.cd('../../');
+    shell.exec('grunt source-all --gargs="-m BUILD_PATH:source/test"');
     shell.cd('source/test');
   });
 
