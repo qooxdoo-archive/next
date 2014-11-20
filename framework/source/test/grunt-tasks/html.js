5eab26c59e5611cc029f34b0804c4bf49f33ebf7
tobiasoberrauch <tobias.oberrauch@1und1.de>
2014-11-20 14:29:09 +0100

    [BUG #8707] Use grunt-injector to enable support for windows and get support from a maintained grunt plugin

--- framework/source/test/grunt-tasks/html.js
+++ framework/source/test/grunt-tasks/html.js
@@ -3,37 +3,44 @@
 var fs = require('fs');
 var glob = require('glob');
 
+var getSourcePaths = function (scope) {
+  if (typeof scope === 'undefined' || scope === 'all') {
+    return 'tests/**/*.js';
+  }
+  return 'tests/' + scope + '/**/*.js';
+};
+
 module.exports = function (grunt) {
-  var getTestPath = function (scope) {
-    if (typeof scope === 'undefined' || scope === 'all') {
-      return 'tests/**/*.js';
+  grunt.loadNpmTasks('grunt-injector');
+  grunt.initConfig({
+    injector: {
+      options: {
+        addRootSlash: false,
+        ignorePath: [
+          'tests/setup.js',
+          'tests/mochaSetup.js',
+          'tests/TestCase.js'
+        ]
+      },
+      test: {
+        files: {
+          // paths will set dynamically
+          "index.html": [],
+          "index-source.html": []
+        }
+      }
     }
-    return 'tests/' + scope + '/**/*.js';
-  };
+  });
 
   grunt.registerTask('html', 'A task to preprocess the website.html', function (scope) {
-    var testPath = getTestPath(scope);
-    // read index files
-    ["index.html", "index-source.html"].forEach(function (fileName) {
-      var index = fs.readFileSync(fileName, {encoding: 'utf8'});
+    var injectorConfig = grunt.config.get('injector');
 
-      var tests = glob.sync(testPath);
-      var scriptTags = '  <!-- TESTS START -->\n';
-      tests.filter(function (path) {
-        // exclude infrastructure files
-        return path.indexOf("setup.js") === -1 &&
-          path.indexOf("mochaSetup.js") === -1 &&
-          path.indexOf("TestCase.js") === -1;
-      })
-        .forEach(function (path) {
-          scriptTags += '  <script src="' + path + '"></script>\n';
-        });
-      scriptTags += '  <!-- TESTS END -->';
-
-      index = index.replace(/\s\s<!--\sTESTS\sSTART\s-->((.|\n)*)<!--\sTESTS\sEND\s-->/g, scriptTags);
-
-      // write index file
-      fs.writeFileSync(fileName, index, {'encoding': 'utf8'});
-    });
+    var sourcePaths = getSourcePaths(scope);
+    injectorConfig.test.files = {
+      "index-source.html": sourcePaths,
+      "index.html": sourcePaths
+    };
+    grunt.config.set('injector', injectorConfig);
+    grunt.task.run(['injector']);
   });
 };
