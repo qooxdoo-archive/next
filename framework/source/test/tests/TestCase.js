40c8e58cb704e96ab35b8c362b219f98bd5fea47
danielwagner <daniel.wagner@1und1.de>
2014-11-20 13:52:53 +0100

    [BUG #8618] fixed skipAfterTest

--- framework/source/test/tests/TestCase.js
+++ framework/source/test/tests/TestCase.js
@@ -3,28 +3,29 @@ var $$root;
 var setUpRoot = function() {
   $$root = new qx.ui.core.Root(document.createElement("div"));
   $$root.appendTo(document.body);
-}
+};
 
 var getRoot = function() {
   return $$root;
-}
+};
 
 var tearDownRoot = function() {
   $$root.empty();
   $$root.dispose();
-}
+};
 
 var skipAfterTest = function(suiteTitle, testTitle) {
-	var suites = q(".suite")
-	for(var i = 0; i < suites.length; i++){
-		if(suiteTitle.indexOf(suites[i].children[0].textContent)===0){
-			suite = suites[i];
+	var suites = q(".suite");
+	for (var i = 0; i < suites.length; i++) {
+		if (suiteTitle.indexOf(suites[i].children[0].textContent) === 0) {
+      qxWeb(suites[i]).find("h2")
+        .filter(function(el) {
+          return el.innerHTML.indexOf(testTitle) !== -1;
+        })
+        .getParents()[0].className = "test pass pending";
 		}
 	}
-  suite.find("h2").filter(function(el) {
-    return el.innerHTML.indexOf(testTitle) !== -1
-  }).getParents()[0].className = "test pass pending";
-}
+};
 
 function assertQxMobileWidget(obj) {
   assert.instanceOf(obj, qx.ui.Widget);
