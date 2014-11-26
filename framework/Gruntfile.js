3972620dafc3fed75f2b310fec4499e1b17ea8c7
danielwagner <daniel.wagner@1und1.de>
2014-11-26 17:13:07 +0100

    [BUG #8717] added api jobs and tasks

--- framework/Gruntfile.js
+++ framework/Gruntfile.js
@@ -14,12 +14,43 @@ module.exports = function(grunt) {
       "APPLICATION" : "qooxdoo",
       "LOCALES": ["en"],
       "QOOXDOO_PATH" : "../",
+    },
+
+    concat: {
+      options: {
+        separator: ';'
+      },
+      samples : {
+        src: ['api/samples/*.js'],
+        dest: 'api/script/samples.js'
+      }
+    },
+
+    watch: {
+      samples: {
+        files: ['api/samples/*.js'],
+        tasks: ['concat:samples']
+      },
+      "api-data": {
+        files: ['../../../framework/source/**/*.js'],
+        tasks: ['api-data']
+      }
     }
   };
 
   var mergedConf = qx.config.mergeConfig(config);
-  // console.log(util.inspect(mergedConf, false, null));
   grunt.initConfig(mergedConf);
 
   qx.task.registerTasks(grunt);
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // 'extend' API job
+  grunt.task.renameTask('api', 'generate-api');
+  grunt.task.registerTask(
+    'api',
+    'Concat the samples and generate the API.',
+    [ "concat:samples", "generate-api"]
+  );
 };
