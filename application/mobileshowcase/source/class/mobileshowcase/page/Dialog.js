5bc704b2a46519aa503ec83a5f0b97eaf78ed246
Sabrina Metzger <sabrina.metzger@1und1.de>
2014-12-17 13:36:08 +0100

    [BUG #8828] added listener for anchor menu

--- application/mobileshowcase/source/class/mobileshowcase/page/Dialog.js
+++ application/mobileshowcase/source/class/mobileshowcase/page/Dialog.js
@@ -119,6 +119,7 @@ qx.Class.define("mobileshowcase.page.Dialog",
       }]);
       this.__anchorMenu = new qx.ui.dialog.Menu(anchorMenuModel, showAnchorMenuButton).appendTo(body);
       this.__anchorMenu.title = "Colors";
+      this.__anchorMenu.on("selected", this.__onAnchorMenuChangeSelection, this);
 
       // BUTTONS
       var showPopupButton = new qx.ui.Button("Popup");
@@ -364,6 +365,15 @@ qx.Class.define("mobileshowcase.page.Dialog",
         "Received <b>changeSelection</b> from Menu Dialog. [index: "
         + index + "] [item: " + model.title + "]");
       this.__menu.hideWithDelay(500);
+    },
+
+    __onAnchorMenuChangeSelection: function(el) {
+      var index = el.getData("row");
+      var model = this.__anchorMenu.model.getItem(index);
+      this.__resultsLabel.value = (
+        "Received <b>changeSelection</b> from Anchor Menu Dialog. [index: " + index + "] [item: " + model.title + "]");
+      this.__anchorMenu.hideWithDelay(500);
     }
+
   }
 });
