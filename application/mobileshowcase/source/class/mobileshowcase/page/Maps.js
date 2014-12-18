523805ad9eb969918f25da92864fb61060bf83ec
Sabrina Metzger <sabrina.metzger@1und1.de>
2014-12-18 16:08:16 +0100

    [BUG #8833] added styles to css file

--- application/mobileshowcase/source/class/mobileshowcase/page/Maps.js
+++ application/mobileshowcase/source/class/mobileshowcase/page/Maps.js
@@ -94,7 +94,6 @@ qx.Class.define("mobileshowcase.page.Maps",
 
       var menuContainer = new qx.ui.Widget();
       menuContainer.setAttribute("id", "mapMenu");
-      menuContainer.setStyles({"top":"10px","right":"10px"})
       // LABEL
       var descriptionLabel = new qx.ui.Label("Page Title");
       descriptionLabel.addClass("osmMapLabel");
@@ -114,7 +113,6 @@ qx.Class.define("mobileshowcase.page.Maps",
       var groupPosition = new qx.ui.form.Group();
       groupPosition.append(this._showMyPositionButton);
       var groupFullScreen = new qx.ui.form.Group();
-      groupFullScreen.setStyle("background","white");
       groupFullScreen.append(descriptionLabel);
       groupFullScreen.append(toggleNavigationButton);
 
