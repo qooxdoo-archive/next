237b0372f88ab78e19d6221a6d32dd0b61255aed
Richard Sternagel <richard.sternagel@1und1.de>
2015-01-15 19:54:14 +0100

    [BUG #8728] align nodeunit tests with new skeleton structure

--- tool/grunt/task/package/dependency/test/data/myapp/Gruntfile.js
+++ tool/grunt/task/package/dependency/test/data/myapp/Gruntfile.js
@@ -1,19 +1,15 @@
 // requires
-var qx = require("../../../../../../../tool/grunt");
+var util = require('util');
+var qx = require("../next.git/tool/grunt");
 
 // grunt
 module.exports = function(grunt) {
   var config = {
 
-    generator_config: {
-      let: {
-      }
-    },
-
     common: {
       "APPLICATION" : "myapp",
-      "QOOXDOO_PATH" : "../../../../../../..",
-      "QXTHEME": "myapp.theme.Theme"
+      "QOOXDOO_PATH" : "../next.git",
+      "THEME": "custom"
     }
 
     /*
@@ -26,10 +22,27 @@ module.exports = function(grunt) {
     */
   };
 
-  var mergedConf = qx.config.mergeConfig(config);
+  var mergedConf = qx.config.mergeConfig(config, {"build": "build-base", "source": "source-base"});
+  // console.log(util.inspect(mergedConf, false, null));
   grunt.initConfig(mergedConf);
 
   qx.task.registerTasks(grunt);
+
+  // 'extend' source job
+  grunt.task.renameTask('source', 'source-base');
+  grunt.task.registerTask(
+    'source',
+    'Build the playground and compile the stylesheets with Sass.',
+    ["sass:indigo", "source-base"]
+  );
+
+  // 'extend' build job
+  grunt.task.renameTask('build', 'build-base');
+  grunt.task.registerTask(
+    'build',
+    'Build the playground and compile the stylesheets with Sass.',
+    ["sass:indigo", "build-base"]
+  );
 
   // grunt.loadNpmTasks('grunt-my-plugin');
 };
