96179c5bbf81bfb0f51b7cedf08ddb30b31e408b
Martin Wittemann <Martin.Wittemann@1und1.de>
2015-01-28 11:27:16 +0100

    [BUG #8744] refactoring and cleanup

--- framework/source/class/qx/ui/FlexCarousel.js
+++ framework/source/class/qx/ui/FlexCarousel.js
@@ -38,12 +38,6 @@ qx.Class.define("qx.ui.FlexCarousel",
       event: true
     },
 
-    showPagination: {
-      check: "Boolean",
-      init: true,
-      apply: "_applyShowPagination"
-    },
-
     pageSwitchDuration: {
       check: "Number",
       init: 500
@@ -52,22 +46,23 @@ qx.Class.define("qx.ui.FlexCarousel",
 
   construct: function(element) {
     this.super(qx.ui.Widget, "construct", element);
-    this.on("trackstart", this._onTrackStart, this)
-    .on("track", this._onTrack, this);
 
-    var outerContainer = new qx.ui.container.Scroll({
-        snap: ".qx-hbox > .flexcarousel-page"
+    this.on("trackstart", this._onTrackStart, this)
+      .on("track", this._onTrack, this);
+
+    qxWeb(window).on("resize", this._onResize, this);
+
+    this.__scrollContainer = new qx.ui.container.Scroll({
+        snap: ".qx-hbox > ." + this.defaultCssClass + "-page"
       })
-      .setStyle("oveflowX", "auto")
-      .setStyle("WebkitOverflowScrolling", "auto") // disable native iOS scrolling
-      .addClass("flexcarousel-container")
+      .addClass(this.defaultCssClass + "-container")
       .appendTo(this)
       .on("trackend", this._onTrackEnd, this);
 
     this.__pageContainer = (new qx.ui.Widget())
       .addClass("qx-hbox")
       .on("removedChild", this._onRemovedChild, this)
-      .appendTo(outerContainer);
+      .appendTo(this.__scrollContainer);
 
     this.__paginationLabels = [];
     this.__pagination = new qx.ui.Widget()
@@ -75,7 +70,7 @@ qx.Class.define("qx.ui.FlexCarousel",
         layout: new qx.ui.layout.HBox(),
         layoutPrefs: {flex: 1}
       })
-      .addClass("flexcarousel-pagination")
+      .addClass(this.defaultCssClass + "-pagination")
       .appendTo(this);
 
     this.on("addedChild", this._onAddedChild, this);
@@ -85,26 +80,29 @@ qx.Class.define("qx.ui.FlexCarousel",
 
   members: {
     __pageContainer: null,
+    __scrollContainer: null,
     __paginationLabels: null,
-    __trackStart: null,
+    __startScrollLeft: null,
+    __pagination: null,
+
 
     nextPage: function(acceleration) {
-      acceleration = acceleration || 1;
-      this._disableEvents();
-      this.find(".flexcarousel-container")
-        .scrollTo(this.getWidth() * 2, 0, parseInt(this.pageSwitchDuration / acceleration))
-        .once("animationEnd", function() {
-          this._enableEvents();
-          this._onScroll();
-        }, this);
+      this._switchPage(acceleration, this.getWidth() * 2);
+      return this;
     },
 
 
     previousPage: function(acceleration) {
+      this._switchPage(acceleration, 0);
+      return this;
+    },
+
+
+    _switchPage : function(acceleration, scrollLeft) {
       acceleration = acceleration || 1;
       this._disableEvents();
-      this.find(".flexcarousel-container")
-        .scrollTo(0, 0, parseInt(this.pageSwitchDuration / acceleration))
+      this.__scrollContainer
+        .scrollTo(scrollLeft, 0, parseInt(this.pageSwitchDuration / acceleration))
         .once("animationEnd", function() {
           this._enableEvents();
           this._onScroll();
@@ -113,29 +111,22 @@ qx.Class.define("qx.ui.FlexCarousel",
 
 
     _enableEvents : function() {
-      if (this.__trackStart === null) {
-        this.find(".flexcarousel-container")
-          .on("scroll", this._onScroll, this);
+      if (this.__startScrollLeft === null) {
+        this.__scrollContainer.on("scroll", this._onScroll, this);
       }
       this.on("swipe", this._onSwipe, this);
     },
 
+
     _disableEvents : function() {
-      this.find(".flexcarousel-container")
-        .off("scroll", this._onScroll, this);
+      this.__scrollContainer.off("scroll", this._onScroll, this);
       this.off("swipe", this._onSwipe, this);
     },
 
 
-    _scrollContainer: function(left, time) {
-      this.find(".flexcarousel-container").scrollTo(left, 0, time);
-    },
-
-
     _onAddedChild: function(child) {
-      child.addClasses(["qx-flex1", "flexcarousel-page"])
-        .appendTo(this.__pageContainer)
-        .setStyle("width", this.getWidth() + "px");
+      child.addClasses(["qx-flex1", this.defaultCssClass + "-page"])
+        .appendTo(this.__pageContainer);
 
       var paginationLabel = this._createPaginationLabel();
       this.__paginationLabels.push(paginationLabel);
@@ -149,10 +140,11 @@ qx.Class.define("qx.ui.FlexCarousel",
         this._update();
       }
 
-      if (this.__pageContainer.find(".flexcarousel-page").length === 3) {
-        this.find(".flexcarousel-container")[0].scrollLeft = this.getWidth();
+      // scroll as soon as we have the third page added
+      if (this.__pageContainer.find("." + this.defaultCssClass + "-page").length === 3) {
+        this.__scrollContainer[0].scrollLeft = this.getWidth();
       }
-      this._updatePagination();
+      this._updatePagination(); // TODO remove?
     },
 
 
@@ -160,7 +152,7 @@ qx.Class.define("qx.ui.FlexCarousel",
       this._updateWidth();
 
       if (this.active[0] == child[0]) {
-        this.active = this.__pageContainer.find(".flexcarousel-page").eq(0);
+        this.active = this.__pageContainer.find("." + this.defaultCssClass + "-page").eq(0);
       } else {
         this._update();
       }
@@ -169,18 +161,22 @@ qx.Class.define("qx.ui.FlexCarousel",
       for (var i = 0; i < this.__paginationLabels.length; i++) {
         this.__paginationLabels[i].getChildren(".label").setHtml((i + 1) + "");
       }
-      this._updatePagination();
+      this._updatePagination(); // TODO remove?
     },
 
 
     _update: function(value, old) {
       var direction = this._updateOrder();
-      var container = this.find(".flexcarousel-container");
-      if (container[0].scrollLeft !== this.getWidth()) {
+
+      if (this.__scrollContainer[0].scrollLeft !== this.getWidth()) {
+        var left;
         if (direction == "right") {
-          this._scrollContainer(container[0].scrollLeft - container.getWidth(), 0);
+          left = this.__scrollContainer[0].scrollLeft - this.__scrollContainer.getWidth();
         } else if (direction == "left") {
-          this._scrollContainer(container[0].scrollLeft + container.getWidth(), 0);
+          left = this.__scrollContainer[0].scrollLeft + this.__scrollContainer.getWidth();
+        }
+        if (left !== undefined) {
+          this.__scrollContainer.scrollTo(left, 0, 0);
         }
       }
 
@@ -188,30 +184,46 @@ qx.Class.define("qx.ui.FlexCarousel",
     },
 
 
+    /**
+     * Updates the CSS order property of the flexbox layout.
+     * The active page should be the second in order with a order property of '0'.
+     * The page left to the active has the order property set to '-1' and is the
+     * only one on the left side. All other pages get increasing order numers and
+     * are alligned on the right side.
+     *
+     * @return {String} The scroll direction, either 'left' or 'right'.
+     */
     _updateOrder: function() {
       var scrollDirection;
-      var pages = this.__pageContainer.find(".flexcarousel-page");
-      var orderBefore = parseInt(this.active.getStyle("order"), 10);
+
+      var pages = this.__pageContainer.find("." + this.defaultCssClass + "-page");
+      var orderBefore = parseInt(this.active.getStyle("order"));
+
       if (orderBefore > 0) {
         scrollDirection = "right";
       } else if (orderBefore < 0) {
         scrollDirection = "left";
       }
+
+      // special case if a third page has been added
       if (orderBefore === 0 && pages.length == 3) {
         scrollDirection = "right";
       }
 
       var activeIndex = pages.indexOf(this.active);
-      this.active.setStyle("order", 0);
+      this.active.setStyle("order", 0); // active page should always have order 0
       var order = 1;
 
+      // order all pages with a higher index than the active page
       for (var i = activeIndex + 1; i < pages.length; i++) {
+        // move the last page to the left of the active page
         if (activeIndex === 0 && i == pages.length - 1) {
           order = -1;
         }
         qxWeb(pages[i]).setStyle("order", order++);
       }
 
+      // order all pages with a lower index than the active page
       for (var i = 0; i < activeIndex; i++) {
         // move the last page to the left of the active page
         if (i == activeIndex - 1) {
@@ -225,22 +237,29 @@ qx.Class.define("qx.ui.FlexCarousel",
 
 
     _updateWidth: function() {
-      var containerWidth = this.getWidth() * this.__pageContainer.find(".flexcarousel-page").length;
+      // set the container width to total width of all pages
+      var containerWidth =
+        this.getWidth() *
+        this.__pageContainer.find("." + this.defaultCssClass + "-page").length;
       this.__pageContainer.setStyle("width", containerWidth + "px");
+      // set the width of all pages to the carousel width
+      this.find("." + this.defaultCssClass + "-page").setStyle("width", this.getWidth() + "px");
     },
 
 
     _onScroll: function() {
-      var container = this.find(".flexcarousel-container");
       var width = this.getWidth();
-      var pages = this.__pageContainer.find(".flexcarousel-page");
-      if (container[0].scrollLeft < (width - width / 2)) {
+      var pages = this.__pageContainer.find("." + this.defaultCssClass + "-page");
+
+      // if more than 50% is visible of the previous page
+      if (this.__scrollContainer[0].scrollLeft < (width - (width / 2))) {
         var prev = this.active.getPrev();
         if (prev.length == 0) {
           prev = pages.eq(pages.length - 1);
         }
         this.active = prev;
-      } else if (container[0].scrollLeft > (width + width / 2)) {
+      // if more than 50% is visible of the next page
+      } else if (this.__scrollContainer[0].scrollLeft > (width + width / 2)) {
         var next = this.active.getNext();
         if (next.length == 0) {
           next = pages.eq(0);
@@ -251,28 +270,34 @@ qx.Class.define("qx.ui.FlexCarousel",
 
 
     _onTrackStart: function(e) {
-      this.__trackStart = this.find(".flexcarousel-container").getProperty("scrollLeft");
-      this.find(".flexcarousel-container")
+      this.__startScrollLeft = this.__scrollContainer.getProperty("scrollLeft");
+      this.__scrollContainer
+        // stop the current scroll animation
         .stop()
-        .off("scroll", this._onScroll, this)
-        .setProperty("scrollLeft", this.__trackStart);
+        // correct the scroll position as the stopped animation
+        // resets to its initial value
+        .setProperty("scrollLeft", this.__startScrollLeft)
+        // do not update on tracking
+        .off("scroll", this._onScroll, this);
     },
 
 
     _onTrack: function(e) {
       if (e.delta.axis == "x") {
-        this._scrollContainer(this.__trackStart - e.delta.x);
+        this.__scrollContainer.scrollTo(this.__startScrollLeft - e.delta.x, 0);
       }
     },
 
 
     _onTrackEnd: function(e) {
-      this.find(".flexcarousel-container").once("animationEnd", function() {
-        if (this.__trackStart === null) {
-          this.find(".flexcarousel-container").on("scroll", this._onScroll, this);
+      // wait until the snap animation ended
+      this.__scrollContainer.once("animationEnd", function() {
+        // enabled the scorll listener if not another track session started
+        if (this.__startScrollLeft === null) {
+          this.__scrollContainer.on("scroll", this._onScroll, this);
         }
       }, this);
-      this.__trackStart = null;
+      this.__startScrollLeft = null;
     },
 
 
@@ -288,37 +313,24 @@ qx.Class.define("qx.ui.FlexCarousel",
     },
 
 
-    _applyShowPagination : function(value, old) {
-      if (value) {
-        if (this.__pageContainer.find(".flexcarousel-page").length > 1) {
-          this.__pagination.show();
-        }
-      } else {
-        this.__pagination.hide();
-      }
-    },
-
-
     /**
      * Factory method for a paginationLabel.
      * @return {qx.ui.Widget} the created pagination label.
      * @param pageIndex {Integer} The page index
      */
     _createPaginationLabel : function(pageIndex) {
-      var paginationIndex = this.__pageContainer.find(".flexcarousel-page").length;
+      var paginationIndex = this.__pageContainer.find("." + this.defaultCssClass + "-page").length;
 
-      var paginationLabel = qxWeb.create('<div class="flexcarousel-pagination-label"></div>')
+      return qxWeb.create('<div class="' + this.defaultCssClass + '-pagination-label"></div>')
         .on("tap", this._onPaginationLabelTap, this)
         .append(qxWeb.create('<div class="label">' + paginationIndex + '</div>'));
-
-      return paginationLabel;
     },
 
 
     _onPaginationLabelTap: function(e) {
-      this.__paginationLabels.forEach(function(label, idx) {
-        if (label[0] === e.currentTarget ) {
-          this.active = this.__pageContainer.find(".flexcarousel-page").eq(idx);
+      this.__paginationLabels.forEach(function(label, index) {
+        if (label[0] === e.currentTarget) {
+          this.active = this.__pageContainer.find("." + this.defaultCssClass + "-page").eq(index);
         }
       }.bind(this));
     },
@@ -330,9 +342,26 @@ qx.Class.define("qx.ui.FlexCarousel",
      * pagination button.
      */
     _updatePagination : function() {
-      this.find(".flexcarousel-pagination-label").removeClass("active");
-      var pages = this.__pageContainer.find(".flexcarousel-page");
+      this.find("." + this.defaultCssClass + "-pagination-label").removeClass("active");
+      var pages = this.__pageContainer.find("." + this.defaultCssClass + "-page");
       this.__paginationLabels[pages.indexOf(this.active)].addClass("active");
+    },
+
+
+    _onResize : function() {
+      this._updateWidth();
+      this.__scrollContainer.refresh(); // refresh snap points
+      this.__scrollContainer.scrollTo(this.getWidth(), 0, 0);
+    },
+
+
+    dispose : function() {
+      // TODO
     }
   }
 });
+
+// TODO flat theme
+// TODO rename
+// TODO test mobile showcase
+// TODO update tests
