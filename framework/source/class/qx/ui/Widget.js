045f4a95ca6436f004c71774e00653ef3ed5c98c
tobiasoberrauch <tobias.oberrauch@1und1.de>
2015-01-29 15:35:41 +0100

    [BUG #8880] Prevent infinite recursion in initWidgets

--- framework/source/class/qx/ui/Widget.js
+++ framework/source/class/qx/ui/Widget.js
@@ -112,6 +112,7 @@ qx.Class.define("qx.ui.Widget", {
     } else {
       this.push(this._createContainerElement());
     }
+    this[0].$$widget = this;
 
     var clazz = qx.ui.Widget;
     if (!this.getAttribute("id")) {
@@ -120,7 +121,6 @@ qx.Class.define("qx.ui.Widget", {
     this.defaultCssClass = undefined;
     this.anonymous = undefined;
     this.activatable = undefined;
-    this[0].$$widget = this;
     this._initDomConfig();
     this.setData("qx-widget", this.classname);
   },
