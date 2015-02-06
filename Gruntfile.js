af024940e7a7df62a102f33c452b0513c7acd022
Richard Sternagel <richard.sternagel@1und1.de>
2015-02-06 14:40:38 +0100

    [BUG #7900] add clean and node_modules removal tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,30 +6,6 @@ module.exports = function(grunt) {
   grunt.initConfig({
     // apps
     run_grunt: {
-      build_play: {
-        options: {
-          log: true,
-          task: ["build"],
-          process: function(res) {
-            if (res.fail) {
-              grunt.log.writeln('Error during "grunt build" within application/play.');
-            }
-          }
-        },
-        src: ['application/play/Gruntfile.js']
-      },
-      build_mobileshowcase: {
-        options: {
-          log: true,
-          task: ["build"],
-          process: function(res) {
-            if (res.fail) {
-              grunt.log.writeln('Error during "grunt build" within application/mobileshowcase.');
-            }
-          }
-        },
-        src: ['application/mobileshowcase/Gruntfile.js']
-      },
       build_apps: {
         options: {
           log: true,
@@ -37,6 +13,18 @@ module.exports = function(grunt) {
           process: function(res) {
             if (res.fail) {
               grunt.log.writeln('Error during building all applications');
+            }
+          }
+        },
+        src: ['application/play/Gruntfile.js', "application/mobileshowcase/Gruntfile.js"]
+      },
+      clean_apps: {
+        options: {
+          log: true,
+          task: ["clean:app"],
+          process: function(res) {
+            if (res.fail) {
+              grunt.log.writeln('Error during cleaning all applications');
             }
           }
         },
@@ -50,9 +38,19 @@ module.exports = function(grunt) {
     'build_skel',
     'Build skeleton app',
     function() {
-      shell.exec('python create-application.py -n "test_skel"');
-      shell.exec('grunt build --gruntfile test_skel/Gruntfile.js');
-      shell.exec('rm -rf test_skel');
+      shell.exec('mkdir -p release');
+      shell.exec('python create-application.py -n test_skel -o release');
+      shell.exec('grunt build --gruntfile release/test_skel/Gruntfile.js');
+      shell.exec('rm -rf release/test_skel');
+    }
+  );
+
+  // rm node_modules
+  grunt.task.registerTask (
+    'remove_node_modules',
+    'Removes all node_modules directories',
+    function() {
+      shell.exec('find . -name "node_modules" | grep -v node_modules/ | grep -v tool/grunt/node_modules | xargs rm -rf');
     }
   );
 
