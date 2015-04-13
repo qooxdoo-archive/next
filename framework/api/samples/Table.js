17ac179954cbb66d053b9e9b52c260f53f19298a
tobiasoberrauch <tobias.oberrauch@1und1.de>
2015-04-13 16:24:09 +0200

    [BUG #9049] Add table example in framework api viewer

--- framework/api/samples/Table.js
+++ framework/api/samples/Table.js
@@ -44,3 +44,37 @@ addSample(".toTable", {
   },
   executable: true
 });
+
+addSample(".toTable", {
+  html: [
+    ' <div class="qx-table-toolbar">',
+    '   <label for="keyword">Filter: </label><input id="keyword" type="text" />',
+    ' </div>',
+    ' <table id="data">',
+    '   <thead>',
+    '     <tr>',
+    '       <th data-qx-table-col-type="String" data-qx-table-col-name="Name">Name</th>',
+    '       <th data-qx-table-col-type="String" data-qx-table-col-name="Position">Position</th>',
+    '       <th data-qx-table-col-type="String" data-qx-table-col-name="Office">Office</th>',
+    '       <th data-qx-table-col-type="Number" data-qx-table-col-name="Age">Age</th>',
+    '       <th data-qx-table-col-type="Numder" data-qx-table-col-name="StartDate">Start date</th>',
+    '       <th data-qx-table-col-type="Number" data-qx-table-col-name="Salary">Salary</th>',
+    '     </tr>',
+    '   </thead>',
+    '   <tbody></tbody>',
+    '</table>'
+  ],
+  javascript: function () {
+    var modelPath = "data/model.json";
+    var request = new q.io.xhr(modelPath);
+    request.on("load", function (event) {
+      var model = JSON.parse(event.responseText);
+
+      var table = q("#data").toTable(model);
+      q("#keyword").on("keyup", function () {
+        table.filter(this.getValue());
+      });
+    });
+    request.send();
+  }
+});
