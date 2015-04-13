1eabe6fd7002cacec3774cde9a2b8d2272a9a034
danielwagner <daniel.wagner@1und1.de>
2015-04-13 11:33:08 +0200

    [BUG #9061] added module configs for Html, Traversing and Event

---
+++ framework/source/class/qx/module/Html.js
@@ -0,0 +1,26 @@
+"use strict";
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2015 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+************************************************************************ */
+
+/**
+ * HTML module
+ *
+ * @require(qx.dom.Element)
+ * @require(qx.dom.Node)
+ * @require(qx.bom.Html)
+ * @require(qx.module.Environment)
+ */
+qx.Class.define("qx.module.Html", {});
