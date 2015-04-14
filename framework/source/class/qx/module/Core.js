7b5e78e7cdfc7839fdc010126590751793852130
danielwagner <daniel.wagner@1und1.de>
2015-04-14 16:12:43 +0200

    [BUG #9061] Core module requires smaller modules

--- framework/source/class/qx/module/Core.js
+++ framework/source/class/qx/module/Core.js
@@ -20,20 +20,37 @@
 
 /**
  * Placeholder class which simply defines and includes the core of qxWeb.
- * The core modules are:
  *
- * * {@link qx.module.Attribute}
- * * {@link qx.module.Css}
- * * {@link qx.module.Environment}
- * * {@link qx.module.Event}
- * * {@link qx.module.Manipulating}
- * * {@link qx.module.Traversing}
- *
+ * @require(qx.module.Oo)
+ * @require(qx.module.Lang)
+ * @require(qxWeb)
+ * @require(qx.module.Environment)
  * @require(qx.module.Attribute)
  * @require(qx.module.Css)
- * @require(qx.module.Environment)
- * @require(qx.module.Event)
  * @require(qx.module.Manipulating)
+ * @require(qx.module.Html)
  * @require(qx.module.Traversing)
+ * @require(qx.module.Event)
+ * @require(qx.module.event.Native)
+ * @require(qx.module.event.Keyboard)
+ * @require(qx.module.event.Mouse)
+ * @require(qx.module.event.TouchHandler)
+ * @require(qx.module.event.Touch)
+ * @require(qx.module.event.PointerHandler)
+ * @require(qx.module.event.Pointer)
+ * @require(qx.module.event.GestureHandler)
+ * @require(qx.module.event.Swipe)
+ * @require(qx.module.event.Tap)
+ * @require(qx.module.event.Pinch)
+ * @require(qx.module.event.Track)
+ * @require(qx.module.event.TrackHandler)
+ * @require(qx.module.event.OrientationHandler)
+ * @require(qx.module.event.Orientation)
+ * @require(qx.module.event.Rotate)
+ * @require(qx.module.util.Array)
+ * @require(qx.module.util.Function)
+ * @require(qx.module.util.Object)
+ * @require(qx.module.util.String)
+ * @require(qx.module.util.Type)
  */
 qx.Class.define("qx.module.Core", {});
