9c7d9ff27bf6a4ddb980b50077aac78153a3805b
tobiasoberrauch <tobias.oberrauch@1und1.de>
2015-04-14 14:27:45 +0200

    [BUG #9074] Delete property only when it's defined

--- framework/source/class/qx/bom/element/Dataset.js
+++ framework/source/class/qx/bom/element/Dataset.js
@@ -45,7 +45,9 @@ qx.Class.define("qx.bom.element.Dataset",
       if (element.dataset) {
         name = qx.lang.String.camelCase(name);
         if ((value === null) || (value === undefined)) {
-          delete element.dataset[name];
+          if (typeof element.dataset[name] !== "undefined") {
+            delete element.dataset[name];
+          }
         } else {
           element.dataset[name] = value;
         }
