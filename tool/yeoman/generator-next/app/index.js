8ec4bbce3f2764cb42b6681e67e5b7e54cb26a72
danielwagner <daniel.wagner@1und1.de>
2015-04-16 14:26:09 +0200

    [BUG #9060] optionally install NPM deps and generate source version

--- tool/yeoman/generator-next/app/index.js
+++ tool/yeoman/generator-next/app/index.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 var assert = require('yeoman-generator').assert;
 var esutils = require('esutils');
 var globals = require('globals');
+var chalk = require('chalk');
 
 module.exports = generators.Base.extend({
 
@@ -18,6 +19,18 @@ module.exports = generators.Base.extend({
     this.option('appnamespace', {
       desc: 'The applications\'s top-level namespace. (Default: The application name)',
       type: String
+    });
+
+    this.option('installDeps', {
+      desc: 'Install NPM dependencies after app creation.',
+      default: false,
+      type: Boolean
+    });
+
+    this.option('generateSource', {
+      desc: 'Generate development version after app creation.',
+      default: false,
+      type: Boolean
     });
   },
 
@@ -42,6 +55,7 @@ module.exports = generators.Base.extend({
   checkNameSpace: function () {
     var split = this.options.appnamespace.split('.');
     split.forEach(function(part) {
+      // check if each namespace part is a valid identifier
       var msg = "Invalid namespace part '" + part + "':";
       assert(esutils.keyword.isIdentifierES6(part, true), msg + " Must be a valid ECMAScript identifier!");
       assert(!esutils.keyword.isRestrictedWord(part, true), msg + " ECMAScript restricted words are not allowed!");
@@ -52,7 +66,9 @@ module.exports = generators.Base.extend({
 
 
   createApplication: function() {
+    // copy and process the template files
     this.directory('.');
+    // force mem-fs to sync so we can access the files
     this.fs.commit(function() {
       var tmplClassDir = path.join(this.destinationRoot(), 'source', 'class', 'custom');
       var appClassDir = path.join(this.destinationRoot(), 'source', 'class', this.options.appnamespace);
@@ -65,6 +81,70 @@ module.exports = generators.Base.extend({
         stylesFrom,
         stylesTo
       );
+
     }.bind(this));
+  },
+
+
+  askForInstall: function() {
+    if (!this.options.installDeps) {
+      var prompt = {
+        type: 'confirm',
+        name: 'installDeps',
+        message: 'Should I install your app\'s module dependencies now?'
+      };
+      var done = this.async();
+      this.prompt(prompt, function(answers) {
+        this.options.installDeps = answers.installDeps;
+        done();
+      }.bind(this));
+    }
+  },
+
+
+  installDeps: function() {
+    if (this.options.installDeps) {
+      this.installDependencies({
+        bower: false,
+        skipMessage: true,
+        callback: function() {
+          this._generateSource();
+        }.bind(this)
+      });
+    }
+  },
+
+
+  askForSource: function() {
+    if (!this.options.installDeps) {
+      this.log(chalk.green("Please remember to run 'npm install' followed by 'grunt source' to generate the development version of your application."));
+    } else if (!this.options.generateSource) {
+      var prompt = {
+        type: 'confirm',
+        name: 'generateSource',
+        message: 'The development version of your app needs to be generated. Should I do that now?'
+      };
+      var done = this.async();
+      this.prompt(prompt, function(answers) {
+        this.options.generateSource = answers.generateSource;
+        done();
+      }.bind(this));
+    }
+  },
+
+
+  _generateSource: function() {
+    if (this.options.installDeps) {
+      if (this.options.generateSource) {
+        var proc = this.spawnCommand('grunt', ['source']);
+        proc.on('close', function (code) {
+          if (code === 0) {
+            this.log(chalk.green("\nYour app is ready. Happy coding!"));
+          }
+        }.bind(this));
+      } else {
+        this.log(chalk.green("Keep in mind you need to run 'grunt source' before you can use your application."));
+      }
+    }
   }
 });
