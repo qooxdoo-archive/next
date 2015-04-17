12e452df56808332539f7fffd8920838954af62f
danielwagner <daniel.wagner@1und1.de>
2015-04-17 12:50:20 +0200

    [BUG #9060] added license header

--- tool/yeoman/generator-next/app/index.js
+++ tool/yeoman/generator-next/app/index.js
@@ -1,3 +1,19 @@
+/* *****************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2015 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+***************************************************************************** */
+
 var generators = require('yeoman-generator');
 var path = require('path');
 var fs = require('fs');
