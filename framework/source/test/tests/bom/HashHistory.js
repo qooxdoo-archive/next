c913e0e0a193a0ccebc3fc56263f02e7c355f519
danielwagner <daniel.wagner@1und1.de>
2015-04-20 14:18:29 +0200

    [BUG #9081] skip test in Firefox

--- framework/source/test/tests/bom/HashHistory.js
+++ framework/source/test/tests/bom/HashHistory.js
@@ -11,6 +11,13 @@ describe("bom.HashHistory", function() {
   });
 
   it("addToHistory", function(){
+    if (qxWeb.env.get("engine.name") == "gecko") {
+      // changing the iframe's URI hash in Firefox causes an exception:
+      // "Unexpected error"  nsresult: "0x8000ffff (NS_ERROR_UNEXPECTED)"
+      // Skip the test since HashHistory is only used for IE9
+      this.test.skipped = true;
+      return;
+    }
     __history.addToHistory("foo","baz");
     sinon.assert.calledOnce(__history._writeState);
   });
